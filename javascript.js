ca7ca978261d9a9d7952781ed422d9bcd17cf5d1
SABRINA LUCIANA VIEIRA PINTO <0030481613035@lti.fatecso.edu.br>
2019-10-09 20:09:19 -0300

    Portifolio Sabrina Vieira outubro/2019 - aula PWeb Denilce

---
+++ javascript.js
@@ -0,0 +1,20 @@
+function openNav(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+//Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").addEventListener('click', function() {
+  console.log('foi clicado')
+})
+
+document.getElementById("defaultOpen").click()
